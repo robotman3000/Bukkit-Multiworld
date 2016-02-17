6147e4a45145e115ff353196901939022a42fa00
robotman3000 <robotman3000@icloud.com>
2016-02-16 20:30:09 -0500

    Added the beginnings of a teleport manager

---
+++ MultiWorld/src/io/github/robotman3000/bukkit/multiworld/teleport/commands/ListPortalsCommand.java
@@ -0,0 +1,15 @@
+package io.github.robotman3000.bukkit.multiworld.teleport.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ListPortalsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
