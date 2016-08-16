18a6e454dbd7dec806e5fd7c9299748eddbeab38
robotman3000 <robotman3000@icloud.com>
2016-08-15 21:58:41 -0400

    Getting ready for the first release of the plugin - Cleaned up the config file options - Added support for activating only when the player is sneaking - Added support for configurable axe items

--- MultiWorld/mods/Treecapitator/io/github/robotman3000/bukkit/spigotplus/mods/treecapitator/mod_Trecapitator.java
+++ MultiWorld/mods/Treecapitator/io/github/robotman3000/bukkit/spigotplus/mods/treecapitator/mod_Trecapitator.java
@@ -1,6 +1,13 @@
 package io.github.robotman3000.bukkit.spigotplus.mods.treecapitator;
 
-import org.bukkit.Bukkit;
+import io.github.robotman3000.bukkit.spigotplus.api.JavaPluginFeature;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -10,77 +17,126 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import io.github.robotman3000.bukkit.spigotplus.api.JavaPluginFeature;
+public class mod_Trecapitator extends JavaPluginFeature implements Listener {
 
-public class mod_Trecapitator extends JavaPluginFeature implements
-		Listener {
-
-	public mod_Trecapitator() {
-		setFeatureName("Treecapitator Mod");
-	}
-
-	@Override
-	public boolean initalize() {
-		Bukkit.getServer().getPluginManager().registerEvents(this, getPlugin());
-		return true;
-	}
+	int logCount = 0;
+	int maxBlocks;
+	private boolean shouldBeSneaking;
+	private int minRemainingDurability;
+	private List<Material> axeItems = new ArrayList<>();
+	private boolean chargeDurability;
+	private boolean chargeDurabilityCreative;
 
 	@EventHandler
 	public void onBlockBroken(BlockBreakEvent event) {
-		if(event.getPlayer().isSneaking()){
-			ItemStack item1 = event.getPlayer().getInventory().getItemInMainHand();
-			ItemStack item2 = event.getPlayer().getInventory().getItemInOffHand();
-			if (itemIsValid(item1) || itemIsValid(item2)) {
-				Block block = event.getBlock();
-				Location loc = block.getLocation();
-				checkBlock((item1 != null ? item1 : item2), loc.getWorld(), loc.getBlockX(), loc.getBlockY(),
-						loc.getBlockZ());
+		// This gets called when a block is broken
+		if(event.getPlayer().hasPermission("spigotplus.treecapitator." + event.getPlayer().getGameMode())){
+			if (event.getPlayer().isSneaking() == shouldBeSneaking){
+				ItemStack item1 = event.getPlayer().getInventory().getItemInMainHand();
+				ItemStack item2 = event.getPlayer().getInventory().getItemInOffHand();
+				if (itemIsValid(item1) || itemIsValid(item2)) {
+					Block block = event.getBlock();
+					Location loc = block.getLocation();
+					ItemStack axeItem = (item1 != null ? item1 : item2);
+					checkBlock(loc.getWorld(), loc.getBlockX(), loc.getBlockY(),
+							loc.getBlockZ(), axeItem);
+					
+					if(logCount >= maxBlocks){
+						event.getPlayer().sendMessage(ChatColor.RED + "You have exceded the maximum number of logs that can be broken at once.");
+					}
+					
+					if(chargeDurability){
+						if((event.getPlayer().getGameMode() != GameMode.CREATIVE) || chargeDurabilityCreative){
+							axeItem.setDurability((short) (axeItem.getDurability() + logCount));
+						}
+					}
+					//event.getPlayer().sendMessage("Logs Broken: " + logCount);
+					logCount = 0;
+				}
 			}
 		}
 	}
-
+	
 	private boolean itemIsValid(ItemStack i) {
 		if (i != null) {
-			switch (i.getType()) {
-			case DIAMOND_AXE:
-			case GOLD_AXE:
-			case IRON_AXE:
-			case STONE_AXE:
-			case WOOD_AXE:
-				return true;
-			default:
-				break;
+			for(Material type : axeItems){
+				if(type.equals(i.getType())){
+					return true;
+				}
 			}
 		}
 		return false;
 
 	}
 
-	private void checkBlock(ItemStack item, World world, int x, int y, int z) {
+	private void checkBlock(World world, int x, int y, int z, ItemStack axe) {
 		Block block = world.getBlockAt(x, y, z);
 		Material type = block.getType();
-		if (type == Material.LOG || type == Material.LOG_2) {
-			if(item != null && (item.getType().getMaxDurability() - item.getDurability()) > 2){
+		if ((type == Material.LOG || type == Material.LOG_2) && (logCount < this.maxBlocks)) {
+			
+			if(!chargeDurability || (((axe.getType().getMaxDurability() - axe.getDurability()) - logCount) > minRemainingDurability)){
 				block.breakNaturally();
-				item.setDurability((short) (item.getDurability() + 1));
-			}
-			for (int xmod = -1; xmod <= 1; xmod++) {
-				for (int ymod = -1; ymod <= 1; ymod++) {
-					for (int zmod = -1; zmod <= 1; zmod++) {
-						checkBlock(item, world, xmod + x, ymod + y, zmod + z);
+				logCount++;
+				for (int xmod = -1; xmod <= 1; xmod++) {
+					for (int ymod = -1; ymod <= 1; ymod++) {
+						for (int zmod = -1; zmod <= 1; zmod++) {
+							checkBlock(world, xmod + x, ymod + y, zmod + z, axe);
+						}
 					}
 				}
 			}
 		}
 	}
 	
-	@Override
-	public int getMinimumMajorVersion() {
-		return 2;
+	protected void loadConfig() {
+		this.maxBlocks = getConfig().getInt(TreeConfigKeys.maxBlocks.name(), 96);
+		this.shouldBeSneaking = getConfig().getBoolean(TreeConfigKeys.shouldBeSneaking.name(), true);
+		this.minRemainingDurability = getConfig().getInt(TreeConfigKeys.minRemainingDurability.name(), 2);
+		this.chargeDurability = getConfig().getBoolean(TreeConfigKeys.chargeDurability.name(), true);
+		this.chargeDurabilityCreative = getConfig().getBoolean(TreeConfigKeys.chargeDurabilityCreative.name(), false);
+		List<String> items = getConfig().getStringList(TreeConfigKeys.axeItems.name());
+		if(items == null){
+			items = Arrays.asList(Material.DIAMOND_AXE.name(), Material.GOLD_AXE.name(), Material.IRON_AXE.name(), Material.STONE_AXE.name(), Material.WOOD_AXE.name());
+			getConfig().set(TreeConfigKeys.axeItems.name(), items);
+		}
+		
+		for(String str : items){
+			Material material = Material.matchMaterial(str);
+			if(material != null){
+				axeItems.add(material);
+			} else {
+				logInfo("No material match was found for \"" + str + "\"");
+			}
+		}
+		
+	}
+	
+	public void saveConfigValues() {
+		getConfig().set(TreeConfigKeys.maxBlocks.name(), this.maxBlocks);
+		getConfig().set(TreeConfigKeys.shouldBeSneaking.name(), this.shouldBeSneaking);
+		getConfig().set(TreeConfigKeys.minRemainingDurability.name(), this.minRemainingDurability);
+		getConfig().set(TreeConfigKeys.chargeDurability.name(), this.chargeDurability);
+		getConfig().set(TreeConfigKeys.chargeDurabilityCreative.name(), this.chargeDurabilityCreative);
 	}
 
 	@Override
-	public int getMinimumMinorVersion() {
+	protected boolean startup() {
+		loadConfig();
+		return true;
+	}
+	
+	@Override
+	protected void shutdown() {
+		saveConfigValues();
+	}
+	
+	@Override
+	public int getRequiredMajorVersion() {
+		return 1;
+	}
+
+	@Override
+	public int getRequiredMinorVersion() {
 		return 0;
 	}
 }
