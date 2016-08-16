1bdbcf3ddc917ddf72040692ae7695e55d26c629
robotman3000 <robotman3000@icloud.com>
2016-08-15 22:05:06 -0400

    Updated the stubs

--- MultiWorld/Teleport/io/github/robotman3000/bukkit/multiworld/teleport/TeleportManager.java
+++ MultiWorld/Teleport/io/github/robotman3000/bukkit/multiworld/teleport/TeleportManager.java
@@ -12,11 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.robotman3000.bukkit.multiworld.teleport.commands.ListPortalsCommand;
 import io.github.robotman3000.bukkit.spigotplus.api.JavaPluginCommand;
+import io.github.robotman3000.bukkit.spigotplus.api.JavaPluginCommandList;
 import io.github.robotman3000.bukkit.spigotplus.api.JavaPluginFeature;
 
 public class TeleportManager extends JavaPluginFeature {
 
-    private enum Commands implements JavaPluginCommand {
+    private enum Commands implements JavaPluginCommandList {
         listportals {
             @Override
             public CommandExecutor getExecutor() {
@@ -36,36 +37,7 @@ public class TeleportManager extends JavaPluginFeature {
         public abstract TabCompleter getTabCompleter();
 
     }
-
-    public TeleportManager() {
-        setFeatureName("Teleport Manager");
-    }
-
-    @Override
-    public boolean initalize() {
-        logInfo("Initializing...");
-        for (Commands cmd : Commands.values()) { // Register Commands
-            logInfo("Registering Command: " + cmd);
-            PluginCommand pCmd = getPlugin().getCommand(cmd.name());
-            pCmd.setExecutor(cmd.getExecutor());
-            pCmd.setTabCompleter(cmd.getTabCompleter());
-        }
-
-        logInfo("Registering Event Handlers");
-        for (Listener evt : getEventHandlers()) {
-            getPlugin().getServer().getPluginManager().registerEvents(evt, getPlugin());
-        }
-        logInfo("Loading Config");
-        loadConfig();
-        return true;
-    }
-
-    @Override
-    protected void loadConfig() {
-        // TODO Auto-generated method stub
-        super.loadConfig();
-    }
-
+    
     @EventHandler
     public void onEntityPortalEvent(EntityPortalEvent evt) {
 
@@ -81,25 +53,13 @@ public class TeleportManager extends JavaPluginFeature {
 
     }
 
-    @Override
-	public void saveConfig() {
-        // TODO Auto-generated method stub
-        super.saveConfig();
-    }
-
-    @Override
-    public void shutdown() {
-        // TODO Auto-generated method stub
-        super.shutdown();
-    }
-    
 	@Override
-	public int getMinimumMajorVersion() {
-		return 2;
+	public int getRequiredMajorVersion() {
+		return 1;
 	}
 
 	@Override
-	public int getMinimumMinorVersion() {
+	public int getRequiredMinorVersion() {
 		return 0;
 	}
 
